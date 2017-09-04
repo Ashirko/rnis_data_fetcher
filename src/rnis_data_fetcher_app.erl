f69ba02bf56368a1d7c68bc7c1dd24212ad9d9bf
AShirko <antonshirko@frtk.ru>
2017-09-04 07:53:58 +0300

    fixed default port and lables

--- src/rnis_data_fetcher_app.erl
+++ src/rnis_data_fetcher_app.erl
@@ -10,8 +10,8 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    Port = application:get_env(rnis_data_fetcher, rnis_connection_port, 9845),
-    Labels = application:get_env(rnis_data_fetcher, data_lables, [<<"all">>]),
+    Port = application:get_env(rnis_data_fetcher, rnis_connection_port, 4017),
+    Labels = application:get_env(rnis_data_fetcher, data_lables, <<"all">>),
     rnis_data_fetcher_sup:start_link(Port,Labels).
 
 stop(_State) ->
