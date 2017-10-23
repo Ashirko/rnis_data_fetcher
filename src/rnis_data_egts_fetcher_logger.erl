061341d16a34ac3afd86b9b5b402dfdcc71b9fc7
egorban <gorban@satissoft.ru>
2017-10-23 14:43:09 +0300

    fixing

--- src/rnis_data_egts_fetcher_logger.erl
+++ src/rnis_data_egts_fetcher_logger.erl
@@ -4,13 +4,18 @@
 -behaviour(gen_server).
 
 %API
--export([start_link/0,stop/0,add_buf/1,to_file_local/2,to_file_global/2]).
+-export([start_link/0,stop/0,add_buf/1]).
 
 %gen_server callback
--export([init/1,terminate/2,handle_cast/2,handle_info/2]).
+-export([init/1,terminate/2,handle_cast/2,handle_info/2,handle_call/3,code_change/3]).
+
+%internal function
+-export([to_file_local/2,to_file_global/2]).
+
 
 -record(state, {timer_ref,buf}).
 -define(FILENAME,"log/rnis_data_egts_fetcher_log.txt").
+
 
 %% ====================================================================
 %% API functions
@@ -29,6 +34,7 @@ add_buf(Data)->
 %%%===================================================================
 init([])->
 	TimePeriod = application:get_env(rnis_data_fetcher, time_period_log),
+	%TimePeriod = 60000,
 	{ok,Ref} = timer:send_interval(TimePeriod, write_log),
 	{ok,#state{timer_ref=Ref,buf=0}}.
 
@@ -38,7 +44,7 @@ handle_cast(_Request,State)->
 	{stop, unknown_message, State}.
 
 handle_info(write_log,#state{buf=Buf}=State)->	
-	<<Year:4,Month:2,Day:2,H:2,M:2,S:2>> = zont_time_util:get_formated_time(),
+	<<Year:4/binary,Month:2/binary,Day:2/binary,H:2/binary,M:2/binary,S:2/binary>> = zont_time_util:get_formated_time(),
 	Time = <<Year/binary,"-",Month/binary,"-",Day/binary," ",H/binary,":",M/binary,":",S/binary>>,		
 	to_file_global(Buf,Time),
 	{noreply,State#state{buf=0}};
