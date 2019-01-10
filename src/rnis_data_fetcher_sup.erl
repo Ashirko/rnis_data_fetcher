5b176eb40635f4ab3ef153e557bb39bea52aca65
egorban <gorban@satissoft.ru>
2019-01-10 11:48:27 +0300

    added start_spec and config for lager

--- src/rnis_data_fetcher_sup.erl
+++ src/rnis_data_fetcher_sup.erl
@@ -3,16 +3,24 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0,start_spec/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 -include_lib("../../rnis_data/include/rnis_data.hrl").
+-include_lib("../../zont_core/include/zont_core_types.hrl").
+
+-define(SERVER, ?MODULE).
+-define(SERVICE,{rnis_data_fetcher,?SERVER}).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
+
+start_spec() ->
+    ?ZONT_NEW_REQ{service = ?SERVICE,
+        request_spec = {{?MODULE, start_link, []}, permanent, 5000, supervisor, [?MODULE]}}.
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
